1c6b9594b50c8528e29cf6b9d26fa126add20f46
José Carlos Nieto <xiam@menteslibres.org>
2013-02-26 14:10:34 -0500

    Updating examples.

--- examples/go/main.go
+++ examples/go/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"github.com/gosexy/yaml"
-	"github.com/gosexy/sugar"
 )
 
 func main() {
 	settings := yaml.New()
 	settings.Set("success", true)
-	settings.Set("nested/tree", 1)
-	settings.Set("another/nested/tree", sugar.List { 1, 2, 3 })
+	settings.Set("nested", "tree", 1)
+	settings.Set("another", "nested", "tree", []int{1, 2, 3})
 	settings.Write("test.yaml")
 }
